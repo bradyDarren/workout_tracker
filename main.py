25d0888158302baa55e8215d972498a8be3c5274
Darren Brady <dcgbfinancial@gmail.com>
2025-04-24 08:03:33 +0000

    addition of the sheety_endpoint

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import requests
 import os
+from datetime import datetime
 
 APPLICATION_ID = os.environ.get("APP_ID")
 APPLICATION_KEY = os.environ.get("APP_KEY")
 
+# connecting to nutritionx api.
 NL_ENDPOINT = 'https://trackapi.nutritionix.com/v2/natural/exercise'
 
 excercise_text = input("Give a description of the activity/excercise you participated in: ")
@@ -21,6 +23,28 @@ w_headers = {
 
 }
 
-workout_post = requests.post(url=NL_ENDPOINT, params=w_params, headers=w_headers)
+workout_post = requests.post(url=NL_ENDPOINT, json=w_params, headers=w_headers)
 results = workout_post.json()
-print(results)
+
+# connecting to Sheety API
+
+sheety_endpoint = 'https://api.sheety.co/9a5f2f38c579638928123fbfa4eab617/workoutsTracking/workouts'
+
+
+date = datetime.now().strftime('%m/%d/%Y')
+time = datetime.now().strftime('%H:%M:%S')
+
+for excercise in results["exercises"]
+    sheet_input = {
+        'workout':{
+            'date': date,
+            'time': time,
+            'excercise': excercise['name'],
+            'duration': excercise['duration_min'],
+            'calories': excercise['nf_calories',]
+
+        }
+    }
+
+    sheet_response = 
+
